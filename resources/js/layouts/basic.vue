8353b377dd4f68ee9f4f516dd403949477db5719
miladev1106 <miladev.net@gmail.com>
2025-08-08 18:46:44 +0700

    update tree folder front end

--- resources/js/layouts/basic.vue
+++ resources/js/layouts/basic.vue
@@ -28,3 +28,63 @@ export default {
 };
 </script>
 
+<style lang="scss">
+
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: normal;
+  margin-top: 0px;
+  font-style: normal;
+  margin-bottom: 20px;
+  color: var(--themeht-text-color);
+  font-family: var(--themeht-typography-secondary-font-family);
+}
+
+h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {
+  color: inherit;
+}
+
+ul {
+  margin-left: 0;
+}
+
+@media (min-width: 1400px) {
+  .container {
+    max-width: 1320px !important;
+  }
+}
+
+a,
+.btn,
+button,
+img,
+#ht-main-nav,
+.main-nav > ul > li > a:before,
+.main-nav .sub-menu li a:before,
+.ht-nav-toggle {
+  -webkit-transition: all 0.5s ease-in-out 0s;
+  -moz-transition: all 0.5s ease-in-out 0s;
+  -ms-transition: all 0.5s ease-in-out 0s;
+  -o-transition: all 0.5s ease-in-out 0s;
+  transition: all 0.5s ease-in-out 0s;
+}
+
+a, a:focus { text-decoration: none !important; }
+a, a:visited { text-decoration: none; color: var(--themeht-text-color);}
+a:focus, a:hover { color: var(--themeht-secondary-color); text-decoration: none !important; }
+a, button, input { outline: medium none !important; }
+
+#particles-js {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
